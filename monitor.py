34d63286a9aec83cdf31f4ad7c01f8b4fd9f1376
jasmesp <jasmes@gmail.com>
2022-07-14 21:10:06 -0400

    Initial commit

---
+++ monitor.py
@@ -0,0 +1,20 @@
+import rtmidi2
+
+
+# output incoming midi data:
+def monitor(midi_port_pick):
+    midi_in = rtmidi2.MidiIn()
+    midi_in.open_port(int(midi_port_pick))
+    while True:
+        x = midi_in.get_message()
+        if x:
+            print(x)
+
+
+# get available devices ask for input:
+in_ports = rtmidi2.get_in_ports()
+out_ports = rtmidi2.get_out_ports()
+print(in_ports, '\n', out_ports)
+midi_choice = input("select device channel")
+
+monitor(midi_choice)  # not sure when to cast to int yet
